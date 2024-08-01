7cf932c52e4aff1ec38bce07ab5acf3d81475a48
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-08-01 18:22:16 +0300

    Busniess Card App

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BusniessCard());
+}
+
+class BusniessCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff2c5ba1),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+                radius: 112,
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                  radius: 110,
+                  backgroundImage: AssetImage('images/tech.png'),
+                )),
+            Text(
+              'Ammar Yasser',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                color: Color(0xFF6C8090),
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              color: Color(0xFF6C8090),
+              thickness: 2,
+              indent: 60,
+              endIndent: 60,
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                height: 65,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xff2c5ba1),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 22),
+                      child: Text(
+                        '01552694437',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                height: 65,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xff2c5ba1),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 22),
+                      child: Text(
+                        'amaryasser046@gmail.com',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
