02ccf71257c6036fd2d3286ca651f114854491f6
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-08-02 14:34:35 +0300

    Busniess Card App

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BusniessCard());
+  runApp(const BusniessCard());
 }
 
 class BusniessCard extends StatelessWidget {
+  const BusniessCard({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xff2c5ba1),
+        backgroundColor: const Color(0xff2c5ba1),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(
+            const CircleAvatar(
                 radius: 112,
                 backgroundColor: Colors.black,
                 child: CircleAvatar(
                   radius: 110,
                   backgroundImage: AssetImage('images/tech.png'),
                 )),
-            Text(
+            const Text(
               'Ammar Yasser',
               style: TextStyle(
                 color: Colors.white,
@@ -28,7 +30,7 @@ class BusniessCard extends StatelessWidget {
                 fontFamily: 'Pacifico',
               ),
             ),
-            Text(
+            const Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
                 color: Color(0xFF6C8090),
@@ -36,69 +38,45 @@ class BusniessCard extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Divider(
+            const Divider(
               color: Color(0xFF6C8090),
               thickness: 2,
               indent: 60,
               endIndent: 60,
               height: 10,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30)),
-                height: 65,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: Icon(
-                        Icons.phone,
-                        size: 32,
-                        color: Color(0xff2c5ba1),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 22),
-                      child: Text(
-                        '01552694437',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    ),
-                  ],
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 32,
+                  color: Color(0xff2c5ba1),
+                ),
+                title: Text(
+                  '01552694437',
+                  style: TextStyle(fontSize: 25),
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30)),
-                height: 65,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: Icon(
-                        Icons.mail,
-                        size: 32,
-                        color: Color(0xff2c5ba1),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 22),
-                      child: Text(
-                        'amaryasser046@gmail.com',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    ),
-                  ],
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50)),
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  size: 32,
+                  color: Color(0xff2c5ba1),
+                ),
+                title: Text(
+                  'amaryasser046@gmail.com',
+                  style: TextStyle(fontSize: 25),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
